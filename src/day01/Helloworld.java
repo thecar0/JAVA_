d70aa99e90fbca5e868115aafb3250d0c4163dfa
unknown <teentoplove23@gmail.com>
2026-03-16 17:30:42 +0900

    day01

---
+++ src/day01/Helloworld.java
@@ -0,0 +1,30 @@
+package day01;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// 실행 메서드
+		// 주석
+		/* 주석
+		 * console : 출력공간
+		 * String : 문자열을 처리하는 클래스
+		 * */
+		
+		System.out.println("Hello world"); //출력방법
+		System.out.println("aaa");
+		
+		char ch ='a'; //반드시 작은따옴표를 사용하여 묶어줘야 함.
+		
+		long lo = 123456555789L; //접미사
+		float f = 1.1f;      //접미사
+		
+		String str = "Hello world";
+		
+		//원래 클래스는 정의하는 방법이 다르지만, String만은 예외로 자료형처럼 정의 가능
+		//대문자로 시작하는 변수는 상수일때의 규칙 (전부대문자)
+		
+		
+
+	}
+
+}
